64af5f36faa9551139a566b300af968ec47c4f0f
Leoo Resende <leosousa1211@gmail.com>
2023-07-16 22:46:27 -0300

    Mudado nomenclaturas.

--- src/components/Saldo.tsx
+++ src/components/Saldo.tsx
@@ -10,7 +10,7 @@ export function Saldo (saldos: Saldos){
 	return (
 		<div className={'saldos-cotainer' + (saldos.saldoPeriodo && saldos.saldoTotal ? '' : ' ocuta')}>
 			<p>Saldo total: R$ {Formatador.formatarMoedaReal(saldos.saldoTotal ?? 0)}</p>
-			<p>Saldo periodo: R$ {Formatador.formatarMoedaReal(saldos.saldoPeriodo ?? 0)}</p>
+			<p>Saldo no periodo: R$ {Formatador.formatarMoedaReal(saldos.saldoPeriodo ?? 0)}</p>
 		</div>
 	);
 }
